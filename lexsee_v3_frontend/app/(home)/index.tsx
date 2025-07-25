de8a189215a4654719c4df60ea7ea802a4b18e26
LeoYang2001 <jya261@uky.edu>
2025-07-25 16:16:48 -0400

    change

--- lexsee_v3_frontend/app/(home)/index.tsx
+++ lexsee_v3_frontend/app/(home)/index.tsx
@@ -5,8 +5,9 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
+  LayoutChangeEvent,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "expo-router";
 import Logo from "~/components/common/Logo";
 import Colors from "~/constants"; // Adjust the import path as necessary
@@ -16,12 +17,72 @@ import InputBox from "~/components/home/InputBox";
 import Dashboard from "~/components/home/Dashboard";
 import { mockWordList } from "~/data/wordslist_mock";
 import FlexCard from "~/components/common/FlexCard";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const Screen = () => {
   const navigation = useNavigation(); // Add this line
 
   const openDrawer = () => {
     navigation.dispatch(DrawerActions.openDrawer()); // Add this function
+  };
+
+  const [ifHideTop, setIfHideTop] = useState(false);
+
+  useEffect(() => {
+    console.log(ifHideTop, "ifHideTop");
+  }, [ifHideTop]);
+
+  const scrollYRef = useRef(0);
+  const lastToggleTimeRef = useRef(Date.now());
+
+  const top = useSharedValue(0);
+  const topHeight = useRef(0);
+
+  // threshold values
+  const SCROLL_THRESHOLD = 15; // Minimum deltaY to consider it fast enough
+  const TIME_THRESHOLD = 150; // Min ms between toggles to avoid jitter
+
+  const handleScroll = (event: any) => {
+    const currentY = event.nativeEvent.contentOffset.y;
+    const deltaY = currentY - scrollYRef.current;
+
+    const now = Date.now();
+    const timeDiff = now - lastToggleTimeRef.current;
+
+    if (Math.abs(deltaY) > SCROLL_THRESHOLD && timeDiff > TIME_THRESHOLD) {
+      if (deltaY > 0) {
+        // scrolling up (content moving up)
+        setIfHideTop(true);
+      } else {
+        // scrolling down (content moving down)
+        setIfHideTop(false);
+      }
+      lastToggleTimeRef.current = now;
+    }
+
+    scrollYRef.current = currentY;
+  };
+
+  // Animate based on state
+  useEffect(() => {
+    top.value = withTiming(ifHideTop ? -topHeight.current : 0, {
+      duration: 300,
+    });
+  }, [ifHideTop]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      top: -100,
+    };
+  });
+
+  const onTopLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+    topHeight.current = height;
   };
 
   return (
@@ -34,32 +95,35 @@ const Screen = () => {
       <View
         style={{
           paddingHorizontal: 18,
+          top: 0,
         }}
         className="flex-1  flex flex-col justify-start "
       >
-        <View className=" w-full flex flex-row justify-between items-center">
-          <Logo />
-          <View className=" flex flex-row gap-4 ">
-            <View className=" flex flex-row items-center gap-1">
-              <Text style={{ color: "#fff", opacity: 0.7 }}>English</Text>
-              <ChevronDown size={18} color={"#fff"} opacity={0.7} />
+        <View onLayout={onTopLayout} style={[animatedStyle]}>
+          <View className=" w-full flex flex-row justify-between items-center">
+            <Logo />
+            <View className=" flex flex-row gap-4 ">
+              <View className=" flex flex-row items-center gap-1">
+                <Text style={{ color: "#fff", opacity: 0.7 }}>English</Text>
+                <ChevronDown size={18} color={"#fff"} opacity={0.7} />
+              </View>
+              <TouchableOpacity onPress={openDrawer} className="  p-1">
+                <Image
+                  style={{
+                    width: 22,
+                    height: 22,
+                  }}
+                  source={require("~/assets/images/menuIcon.png")}
+                />
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={openDrawer} className="  p-1">
-              <Image
-                style={{
-                  width: 22,
-                  height: 22,
-                }}
-                source={require("~/assets/images/menuIcon.png")}
-              />
-            </TouchableOpacity>
           </View>
-        </View>
-        <View className="mt-7">
-          <InputBox />
-        </View>
-        <View className="mt-7">
-          <Dashboard />
+          <View className="mt-7">
+            <InputBox />
+          </View>
+          <View className="mt-7">
+            <Dashboard />
+          </View>
         </View>
         <View className="mt-7">
           <Text
@@ -71,7 +135,11 @@ const Screen = () => {
             Recently Pinned
           </Text>
         </View>
-        <ScrollView className="mt-4 flex-1 ">
+        <ScrollView
+          scrollEventThrottle={16}
+          onScroll={handleScroll}
+          className="mt-4 flex-1  border border-white"
+        >
           <View className="flex-col flex-wrap gap-2">
             {mockWordList.map((word) => (
               <FlexCard
--- lexsee_v3_frontend/components/home/Dashboard.tsx
+++ lexsee_v3_frontend/components/home/Dashboard.tsx
@@ -1,55 +1,173 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+interface DashboardProps {
+  ifReviewInfo?: boolean;
+}
+
+interface ReviewInfoProps {
+  ifReviewInfo: {
+    toBeReviewed: number;
+    reviewed: number;
+  };
+}
+const ReviewInfoComponent = ({ ifReviewInfo }: ReviewInfoProps) => {
+  return (
+    <View className="flex-1 flex-col items-center justify-center">
+      <View>
+        <View>
+          <Text className="text-white">
+            To Be Reviewed: {ifReviewInfo.toBeReviewed}
+          </Text>
+          <Text className="text-white">Reviewed: {ifReviewInfo.reviewed}</Text>
+        </View>
+        <Text>to review today</Text>
+      </View>
+    </View>
+  );
+};
 
 const Dashboard = () => {
+  const [ifReviewInfo, setIfReviewInfo] = useState(true);
+
+  const toggleReviewInfo = () => {
+    setIfReviewInfo((prev) => !prev);
+  };
+
+  const height = useSharedValue(100);
+  const opacity = useSharedValue(0);
+
+  const mockReviewInfo = {
+    toBeReviewed: 15,
+    reviewed: 17,
+  };
+
+  const duration = 250; // Duration for the animations
+  useEffect(() => {
+    if (ifReviewInfo) {
+      height.value = withTiming(200, { duration: duration });
+      opacity.value = withTiming(1, { duration: duration });
+    } else {
+      height.value = withTiming(100, { duration: duration });
+      opacity.value = withTiming(0, { duration: duration });
+    }
+  }, [ifReviewInfo]);
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      height: height.value,
+    };
+  });
+
+  const bottomViewStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   return (
-    <LinearGradient
-      // Button Linear Gradient
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
-      style={{
-        height: 100,
-        borderRadius: 12,
-      }}
-      colors={["#f73d02", "#fe511a"]}
-      className="relative flex flex-row items-center justify-between px-4"
+    <Animated.View
+      style={[
+        containerStyle,
+        {
+          overflow: "hidden",
+          position: "relative",
+          justifyContent: "flex-start",
+        },
+      ]}
     >
-      <Image
-        className="absolute right-0"
-        style={{ width: 140, height: 110 }}
-        source={require("../../assets/images/dashboardBg.png")}
-      />
-      <View className="flex w-full h-full flex-row items-center gap-4 px-4">
-        <TouchableOpacity
-          className=" flex-1 flex h-full flex-col items-start   justify-center gap-1"
-          style={{ zIndex: 1 }}
-        >
-          <Text style={{ fontSize: 28 }} className="text-white font-semibold">
-            3
-          </Text>
-          <Text className="text-white opacity-80">Word</Text>
-        </TouchableOpacity>
-        <View
+      <TouchableOpacity
+        onPress={toggleReviewInfo}
+        style={{
+          zIndex: 10,
+        }}
+      >
+        <LinearGradient
+          // Button Linear Gradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={["#f73d02", "#fe511a"]}
           style={{
-            width: 1,
-            height: 12,
-            opacity: 0.2,
-            backgroundColor: "#fff",
-            borderRadius: 1,
+            height: 100,
+            borderRadius: 12,
           }}
-        />
-        <TouchableOpacity
-          className=" flex-1 flex h-full flex-col items-start   justify-center gap-1"
-          style={{ zIndex: 1 }}
+          className="relative flex flex-row items-center justify-between px-4"
         >
-          <Text style={{ fontSize: 28 }} className="text-white font-semibold">
-            6
-          </Text>
-          <Text className="text-white opacity-80">Story</Text>
-        </TouchableOpacity>
-      </View>
-    </LinearGradient>
+          <Image
+            className="absolute right-0"
+            style={{ width: 140, height: 110 }}
+            source={require("../../assets/images/dashboardBg.png")}
+          />
+          <View className="flex w-full h-full flex-row items-center gap-4 px-4">
+            <TouchableOpacity
+              className=" flex-1 flex h-full flex-col items-start   justify-center gap-1"
+              style={{ zIndex: 1 }}
+            >
+              <Text
+                style={{ fontSize: 28 }}
+                className="text-white font-semibold"
+              >
+                3
+              </Text>
+              <Text className="text-white opacity-80">Word</Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                width: 1,
+                height: 12,
+                opacity: 0.2,
+                backgroundColor: "#fff",
+                borderRadius: 1,
+              }}
+            />
+            <TouchableOpacity
+              className=" flex-1 flex h-full flex-col items-start   justify-center gap-1"
+              style={{ zIndex: 1 }}
+            >
+              <Text
+                style={{ fontSize: 28 }}
+                className="text-white font-semibold"
+              >
+                6
+              </Text>
+              <Text className="text-white opacity-80">Story</Text>
+            </TouchableOpacity>
+          </View>
+        </LinearGradient>
+      </TouchableOpacity>
+      <Animated.View
+        style={[
+          bottomViewStyle,
+          {
+            top: -10,
+            zIndex: 0,
+          },
+        ]}
+        className="  flex-1"
+      >
+        <LinearGradient
+          // Button Linear Gradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={["#292526", "#572e24", "#292526"]}
+          style={{
+            height: "100%",
+            borderBottomLeftRadius: 12,
+            borderBottomRightRadius: 12,
+            zIndex: 10,
+          }}
+          className="relative flex flex-row items-center justify-between px-4"
+        >
+          <ReviewInfoComponent ifReviewInfo={mockReviewInfo} />
+        </LinearGradient>
+      </Animated.View>
+    </Animated.View>
   );
 };
 
